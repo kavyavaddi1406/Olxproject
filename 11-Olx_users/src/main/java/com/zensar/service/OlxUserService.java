647264ada9520ab0a69d3c5ec44fc69b8777d3ab
kavyavaddi1406 <104185858+kavyavaddi1406@users.noreply.github.com>
2022-06-15 16:32:05 +0530

    Add files via upload

---
+++ 11-Olx_users/src/main/java/com/zensar/service/OlxUserService.java
@@ -0,0 +1,40 @@
+package com.zensar.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zensar.bean.OlxUser;
+import com.zensar.db.OlxUserDAO;
+
+
+
+
+
+@Service
+public class OlxUserService {
+@Autowired
+	OlxUserDAO dao;
+	
+	public OlxUser addOlxUser(OlxUser olxUser) {
+		return dao.save(olxUser);
+		
+	}
+	
+	public OlxUser findByUserName(String username) {
+		return this.dao.findByUserName(username);
+		
+	}
+	
+	public OlxUser findUserById(int id) {
+		Optional<OlxUser> optional=this.dao.findById(id);
+		if(optional.isPresent())
+			return optional.get();
+		return null;
+	}
+
+}
+	
+	
+
